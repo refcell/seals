924e9fdd5d7e0c7b95c284766a69cc217af12b3b
Andreas Bigger <abigger87@gmail.com>
2022-02-10 11:58:13 -0800

    :hammer: start coffer impl

--- src/Coffer.sol
+++ src/Coffer.sol
@@ -5,14 +5,42 @@ import {Auth} from "@solmate/auth/Auth.sol";
 
 /// @title Coffer
 /// @notice A Lockbox Contract that stores auction collaterals
-/// @notice Uses Auth patterns as demonstrated in https://github.com/Rari-Capital/vaults/blob/main/src/Vault.sol
+/// @dev Uses Auth patterns as demonstrated in https://github.com/Rari-Capital/vaults/blob/main/src/Vault.sol
+/// @dev Adapted from Lil Gnosis in https://github.com/m1guelpf/lil-web3
 /// @author Andreas Bigger <andreas@nascent.xyz>
 contract Coffer is Auth {
-	/// @notice Prevents unauthorized token minting or burning
-	error Unauthorized();
+
+  /// >>>>>>>>>>>>>>>>>>>>  CUSTOM ERRORS  <<<<<<<<<<<<<<<<<<<<< ///
+
+	/// @notice Thrown when the provided signatures are invalid, duplicated, or out of order
+	error InvalidSignatures();
+
+	/// @notice Thrown when the execution of the requested transaction fails
+	error ExecutionFailed();
+
+  /// >>>>>>>>>>>>>>>>>>>>  CUSTOM EVENTS  <<<<<<<<<<<<<<<<<<<<< ///
+
+	/// @notice Emitted when the number of required signatures is updated
+	/// @param newQuorum The new amount of required signatures
+	event QuorumUpdated(uint256 newQuorum);
+
+	/// @notice Emitted when a new transaction is executed
+	/// @param target The address the transaction was sent to
+	/// @param value The amount of ETH sent in the transaction
+	/// @param payload The data sent in the transaction
+	event Executed(address target, uint256 value, bytes payload);
+
+	/// @notice Emitted when a new signer gets added or removed from the trusted signers
+	/// @param signer The address of the updated signer
+	/// @param shouldTrust Wether the contract will trust this signer going forwards
+	event SignerUpdated(address indexed signer, bool shouldTrust);
+
+  /// >>>>>>>>>>>>>>>>>>>>>  IMMUTABLES  <<<<<<<<<<<<<<<<<<<<<<< ///
 
 	/// @notice The Seal that manages this Coffer
 	address public immutable seal;
+
+  /// >>>>>>>>>>>>>>>>>>>>>  CONSTRUCTOR  <<<<<<<<<<<<<<<<<<<<<< ///
 
 	/// @notice Deploys Coffer
   /// @dev Initiates the Auth Module with seal_ as the sole authority
@@ -25,5 +53,6 @@ contract Coffer is Auth {
     )
   {}
 
-  // TODO: deposits and withdrawals
+  /// @notice Can deposit to this contract
+  function 
 }
