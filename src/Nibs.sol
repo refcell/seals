0ef6a5f05bc8329e28ea5b498161b344f57f7fc5
Andreas Bigger <abigger87@gmail.com>
2022-02-10 11:37:51 -0800

    :gear: nibs

--- src/Nibs.sol
+++ src/Nibs.sol
@@ -5,40 +5,40 @@ import {ERC20} from "@solmate/tokens/ERC20.sol";
 
 /// @title Nibs
 /// @notice Project Shares for a given Seal
+/// @notice Adapted from LilJuicebox in https://github.com/m1guelpf/lil-web3
 /// @author Andreas Bigger <andreas@nascent.xyz>
 contract Nibs is ERC20{
-	/// ERRORS ///
-
-	/// @notice Thrown when trying to directly call the mint or burn functions
+	/// @notice Prevents unauthorized token minting or burning
 	error Unauthorized();
 
-	/// @notice The manager of this campaign
-	address public immutable manager;
+	/// @notice The Seal that maps to this nibs
+	address public immutable seal;
 
-	/// @notice Deploys a ProjectShare instance with the specified name and symbol
+	/// @notice Deploys Nibs with the name and symbol metadata
 	/// @param name The name of the deployed token
 	/// @param symbol The symbol of the deployed token
+  /// @param seal_ The Seal that manages this contract
 	/// @dev Deployed from the constructor of the LilJuicebox contract
-	constructor(string memory name, string memory symbol) payable ERC20(name, symbol, 18) {
-		manager = msg.sender;
+	constructor(string memory name, string memory symbol, address seal_) payable ERC20(name, symbol, 18) {
+		seal = seal_;
 	}
 
 	/// @notice Grants the specified address a specified amount of tokens
+	/// @dev This function will revert if not called from Seal
 	/// @param to The address that will receive the tokens
 	/// @param amount the amount of tokens to receive
-	/// @dev This function should be called from within LilJuicebox, and will revert if manually accessed
 	function mint(address to, uint256 amount) public payable {
-		if (msg.sender != manager) revert Unauthorized();
+		if (msg.sender != seal) revert Unauthorized();
 
 		_mint(to, amount);
 	}
 
 	/// @notice Burns a specified amount of tokens from a specified address' balance
+  /// @dev This function will revert if not called from Seal
 	/// @param from The address that will get their tokens burned
 	/// @param amount the amount of tokens to burn
-	/// @dev This function should be called from within LilJuicebox, and will revert if manually accessed
 	function burn(address from, uint256 amount) public payable {
-		if (msg.sender != manager) revert Unauthorized();
+		if (msg.sender != seal) revert Unauthorized();
 
 		_burn(from, amount);
 	}
